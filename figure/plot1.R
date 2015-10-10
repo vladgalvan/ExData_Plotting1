d8808df2a653c0fc865c540c0039eb83cab15cb8
Vladimir <vgalvan@axtel.com.mx>
2015-10-10 15:29:04 -0500

    load course project 1 files!

---
+++ figure/plot1.R
@@ -0,0 +1,22 @@
+Sys.setlocale("LC_TIME", "English")
+
+#Download and unzip file#
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+unzip(temp)
+unlink(temp)
+
+#Subset data from requested dates#
+rawdata <- read.table("./household_power_consumption.txt",header=TRUE, sep = ";", na.strings = c("?",""))
+rawdata$Date <- as.Date(rawdata$Date, format = "%d/%m/%Y")
+predatapower<- subset(rawdata, Date == "2007-02-01" | Date == "2007-02-02")
+
+#create a date/time column#
+DT<-as.POSIXct(paste(predatapower$Date, predatapower$Time), format="%Y-%m-%d %H:%M:%S")
+datapower<-cbind(DT,predatapower[,3:9])
+
+##create the plot and save it to PNG 480x480 (default) file#
+png("plot1.png", width = 480, height=480, type="window")
+par(ps=12)
+hist(datapower$Global_active_power,breaks = 16,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power",ps=9)
+dev.off()
---
+++ figure/plot4.R
@@ -0,0 +1,40 @@
+#Download and unzip file#
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+unzip(temp)
+unlink(temp)
+Sys.setlocale("LC_TIME", "English")
+
+#Subset data from requested dates#
+rawdata <- read.table("./household_power_consumption.txt",header=TRUE, sep = ";", na.strings = c("?",""))
+rawdata$Date <- as.Date(rawdata$Date, format = "%d/%m/%Y")
+predatapower<- subset(rawdata, Date == "2007-02-01" | Date == "2007-02-02")
+
+#create a date/time column#
+DT<-as.POSIXct(paste(predatapower$Date, predatapower$Time), format="%Y-%m-%d %H:%M:%S")
+datapower<-cbind(DT,predatapower[,3:9])
+
+##create the plot and save it to PNG 480x480 (default) file#
+png("plot4.png", width = 480, height=480, type="window")
+par(mfrow=c(2,2))
+par(ps=12)
+with(datapower,plot(DT, Global_active_power, type = "l", xlab = "",ylab = "Global Active Power"))
+with(datapower,plot(DT, Voltage, type = "l", xlab = "datetime",ylab = "Voltage"))
+with(datapower,plot(DT, Sub_metering_1, type = "l", xlab = "",ylab = "Energy sub metering"))
+lines(DT, datapower$Sub_metering_2, col="red")
+lines(DT, datapower$Sub_metering_3, col="blue")
+legend("topright", pch = "_", col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),bty = "n")
+with(datapower,plot(DT, Global_reactive_power, type = "l", xlab = "datetime"))
+dev.off()
+
+
+
+
+
+png("plot3.png", width = 480, height=480, type="window")
+par(ps=12)
+with(datapower,plot(DT, Sub_metering_1, type = "l", xlab = "",ylab = "Energy sub metering"))
+lines(DT, datapower$Sub_metering_2, col="red")
+lines(DT, datapower$Sub_metering_3, col="blue")
+legend("topright", pch = "_", col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
