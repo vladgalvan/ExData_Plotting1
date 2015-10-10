618d9b5b0d4b71bbf5d71707bd32fd9c529525df
Vladimir <vgalvan@axtel.com.mx>
2015-10-10 16:06:33 -0500

    load course project 1 files!

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,5 +1,3 @@
-Sys.setlocale("LC_TIME", "English")
-
 #Download and unzip file#
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
@@ -15,7 +13,8 @@ predatapower<- subset(rawdata, Date == "2007-02-01" | Date == "2007-02-02")
 DT<-as.POSIXct(paste(predatapower$Date, predatapower$Time), format="%Y-%m-%d %H:%M:%S")
 datapower<-cbind(DT,predatapower[,3:9])
 
-##create the plot and save it to PNG 480x480 (default) file#
+##create the plot and save it to PNG 480x480 file#
+Sys.setlocale("LC_TIME", "English")
 png("plot1.png", width = 480, height=480, type="window")
 par(ps=12)
 hist(datapower$Global_active_power,breaks = 16,col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power",ps=9)
--- figure/plot3.R
+++ figure/plot3.R
@@ -1,5 +1,3 @@
-Sys.setlocale("LC_TIME", "English")
-
 #Download and unzip file#
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
@@ -15,11 +13,12 @@ predatapower<- subset(rawdata, Date == "2007-02-01" | Date == "2007-02-02")
 DT<-as.POSIXct(paste(predatapower$Date, predatapower$Time), format="%Y-%m-%d %H:%M:%S")
 datapower<-cbind(DT,predatapower[,3:9])
 
-##create the plot and save it to PNG 480x480 (default) file#
+##create the plot and save it to PNG 480x480 file#
+Sys.setlocale("LC_TIME", "English")
 png("plot3.png", width = 480, height=480, type="window")
 par(ps=12)
 with(datapower,plot(DT, Sub_metering_1, type = "l", xlab = "",ylab = "Energy sub metering"))
 lines(DT, datapower$Sub_metering_2, col="red")
 lines(DT, datapower$Sub_metering_3, col="blue")
-legend("topright", pch = "_", col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 2)
 dev.off()
